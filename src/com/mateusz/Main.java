ea6f15566c0caeb7d7bb8118555f760d8879daff
Moohen <mateusz.czekala.masazysta@gmail.com>
2018-01-27 19:21:49 +0100

    Initial commit

---
+++ src/com/mateusz/Main.java
@@ -0,0 +1,56 @@
+package com.mateusz;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int[] myIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(myIntegers);
+        sortIntegers(sorted);
+
+    }
+
+    private static int[] getIntegers(int capacity) {
+        System.out.println("Enter " + capacity + " integers value.\r");
+        int[] array = new int[capacity];
+
+        for(int i=0; i<array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        return array;
+    }
+
+    private static void printArray(int[] array) {
+        for(int i=0; i<array.length; i++) {
+            System.out.println("Element " + i + " contents " + array[i]);
+        }
+    }
+
+    private static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for(int i=0; i<array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for(int i=0; i<sortedArray.length-1; i++) {
+                if(sortedArray[i] < sortedArray[i+1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+
+        return sortedArray;
+    }
+
+}
